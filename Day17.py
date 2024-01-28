bef8c1586e7f65efb4b154dea43f45b0d6d20919
Jessica Wang <xuecen.wang@gmail.com>
2024-01-27 21:40:20 -0500

    Day17 completed

--- Day17.py
+++ Day17.py
@@ -1,1 +1,7 @@
 #Create a program that capitalizes the first letter of each word in a sentence
+#i: string; o: string
+def capitalizes_first(string):
+    return string.title()
+
+# print(capitalizes_first("Hello, my name is python."))
+# print(capitalizes_first("hello world! join the community. "))
